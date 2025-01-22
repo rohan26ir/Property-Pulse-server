e1ffab77e601648a0d3d5aac3be955b25d6de0bf
Rohan <rohan26ir@gmail.com>
2025-01-22 21:57:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const port = process.env.PORT || 8000;
+const app = express();
+const cookieParser = require('cookie-parser');
+
+const corsOptions = {
+  origin: ['http://localhost:5173',
+          'https://content-hub-24.web.app',
+          'https://content-hub-24.firebaseapp.com',
+  ], 
+  credentials: true,
+  methods: ['GET', 'POST', 'DELETE', 'PUT'],
+  optionalSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8c67l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+
+
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('PropertyPulse is running....');
+});
+
+app.listen(port, () => console.log(`PropertyPulse running on port ${port}`));
