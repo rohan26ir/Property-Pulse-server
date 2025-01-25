e96c6d5da780f9a9d76ad52961a0287c56c459c4
Rohan <rohan26ir@gmail.com>
2025-01-25 18:39:11 +0600

    Get admin role  user

--- index.js
+++ index.js
@@ -102,6 +102,22 @@ async function run() {
       }
     });
 
+    // Get user by email
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
+
 
 
 
